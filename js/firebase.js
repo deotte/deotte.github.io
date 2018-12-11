6285680a3f106380a3afcad12d0583d8bef73494
deotte <derrickotte@gmail.com>
2018-12-11 18:55:04 -0500

    Remove uneccessary Jquery, Morphtext, Firebase, and XML files from PHPStorm

--- js/firebase.js
+++
@@ -1,8 +0,0 @@
-const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyDBkPZ91_e51lS0QiXUc0tJu4vXswiHZv4",
-    authDomain: "portfolio-contact-1b674.firebaseapp.com",
-    databaseURL: "https://portfolio-contact-1b674.firebaseio.com",
-    projectId: "portfolio-contact-1b674",
-    storageBucket: "portfolio-contact-1b674.appspot.com",
-    messagingSenderId: "803663831462"
-};
